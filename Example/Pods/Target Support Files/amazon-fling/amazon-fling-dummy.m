f6264b1caacf425a1a41a95fc24c483fab6eedc5
Colin Ryan <cryan@digisoft.tv>
2019-03-07 17:48:00 +0000

    Add podspec for AmazonFling framework

---
+++ Example/Pods/Target Support Files/amazon-fling/amazon-fling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_amazon_fling : NSObject
+@end
+@implementation PodsDummy_amazon_fling
+@end
---
+++ Frameworks/AmazonFling.framework/Versions/A/Headers/AmazonFling.h
@@ -0,0 +1,18 @@
+/**
+ *  AmazonFling.h
+ *
+ * Copyright (c) 2015-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * Use is subject to license terms.
+ */
+
+#define AMAZONFLING_VERSION @"1.3.4"
+
+#import "DiscoveryController.h"
+#import "InstallDiscoveryController.h"
+#import "MediaPlayerInfo.h"
+#import "MediaPlayerStatus.h"
+#import "RemoteInstallService.h"
+#import "RemoteMediaPlayer.h"
